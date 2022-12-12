96dca5dd58cc1a1c8d4147abc8895208f76fb59e
Дима <@DESKTOP-TC925PR>
2022-12-12 22:29:30 +0200

    Add project files.

---
+++ 11.12/11.12.cpp
@@ -0,0 +1,85 @@
+﻿//Main.cpp
+#include <iostream>
+#include "Socket.h"
+
+using namespace std;
+
+int main()
+{
+	system("chcp 1251");
+	system("cls");
+
+	int nChoice;
+	int port = 24242;  //âûáèðàåì ïîðò
+	string ipAddress = "178.212.194.169"; //Àäðåñ ñåðâåðà
+	//string ipAddress = "10.4.3.8";
+	char receiveMessage[MAXSTRLEN];
+	char sendMessage[MAXSTRLEN];
+
+	cout << "1) Start server" << endl;
+	cout << "2) Connect to server" << endl;
+	cout << "3) Exit" << endl;
+	cin >> nChoice;
+	if (nChoice == 1)
+	{
+		ServerSocket server;
+		cout << "Starting server..." << endl;
+		//Çàïóñêàåì ñåðâåð
+		server.StartHosting(port);
+		while (true)
+		{
+			cout << "\tWaiting..." << endl;
+			//Ïîëó÷àåì äàííûå îò êëèåíòà
+			//è ñîõðàíÿåì â ïåðåìåííîé receiveMessage
+			server.ReceiveData(receiveMessage, MAXSTRLEN);
+
+			if (receiveMessage[0] == '1') {
+				cout << "Òåëåôîí" << endl;
+				cout << "Êîë-âî: " << receiveMessage[2] << endl;
+			}
+			else if (receiveMessage[0] == '2') {
+				cout << "Íîóòáóê" << endl;
+				cout << "Êîë-âî: " << receiveMessage[2] << endl;
+			}
+			else if (receiveMessage[0] == '3') {
+				cout << "Ïëàíøåò" << endl;
+				cout << "Êîë-âî: " << receiveMessage[2] << endl;
+			}
+			//Îòïðàâëÿåì äàííûå êëèåíòó
+			server.SendDataMessageToClient(receiveMessage);
+			//Åñëè åñòü ñîîáùåíèå "end", çàâåðøàåì ðàáîòó
+			if (strcmp(receiveMessage, "end") == 0 ||
+				strcmp(sendMessage, "end") == 0)
+				break;
+		}
+	}
+	else if (nChoice == 2)
+	{
+		cout << "1) Клава - 7800 ãðí" << endl;
+		cout << "2) Наушники - 26000 ãðí" << endl;
+		cout << "3) Мышь - 9000 ãðí" << endl;
+
+		//cout << "Enter an IP address: " << endl;
+		//Çàïðàøèâàåì IP ñåðâåðà
+		//cin >> ipAddress;
+		ClientSocket client;
+		//ïîäêëþ÷àåìñÿ ê ñåðâåðó
+		client.ConnectToServer(ipAddress.c_str(), port);
+		while (true)
+		{
+			//îòïðàâëÿåì ñîîáùåíèå
+			client.SendDataMessage();
+			cout << "\tWaiting" << endl;
+			//ïîëó÷àåì îòâåò
+			client.ReceiveData(receiveMessage, MAXSTRLEN);
+			cout << "Received: " << receiveMessage << endl;
+			if (strcmp(receiveMessage, "end") == 0 ||
+				strcmp(sendMessage, "end") == 0)
+				break;
+		}
+		//Çàêðûâàåì ñîåäèíåíèå
+		client.CloseConnection();
+	}
+	else if (nChoice == 3)
+		return 0;
+}
---
+++ 11.12/socket.cpp
@@ -0,0 +1,162 @@
+﻿#include "Socket.h"
+
+Socket::Socket()
+{
+	//åñëè èíèöèàëèçàöèÿ ñîêåòîâ ïðîøëà íåóñïåøíî, âûâîäèì ñîîáùåíèå îá
+	//îøèáêå
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != NO_ERROR) // ðåãèñòðàöèÿ
+	{
+		cout << "WSAStartup error\n";
+		system("pause");
+		WSACleanup();
+		exit(10);
+	}
+
+	//ñîçäàåì ïîòîêîâûé ñîêåò, ïðîòîêîë TCP
+	_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	//ïðè íåóñïåøíîì ñîçäàíèè ñîêåòà âûâîäèì ñîîáùåíèå îá îøèáêå
+	if (_socket == INVALID_SOCKET)
+	{
+		cout << "Socket create error." << endl;
+		system("pause");
+		WSACleanup();
+		exit(11);
+	}
+
+
+}
+
+Socket::~Socket()
+{
+	WSACleanup();//î÷èùàåì ðåñóðñû
+}
+
+bool Socket::SendData(char* buffer)
+{
+	/*Îòïðàâëÿåì ñîîáùåíèå íà óêàçàííûé ñîêåò*/
+	send(_socket, buffer, strlen(buffer), 0);
+	return true;
+}
+
+bool Socket::ReceiveData(char* buffer, int size)
+{
+
+	/*Ïîëó÷àåì ñîîáùåíèå è ñîõðàíÿåì åãî â áóôåðå.
+	Ìåòîä ÿâëÿåòñÿ áëîêèðóþùèì!*/
+	int i = recv(_socket, buffer, size, 0);
+	buffer[i] = '\0';
+	return true;
+}
+
+void Socket::CloseConnection()
+{
+	//Çàêðûâàåì ñîêåò
+	closesocket(_socket);
+}
+
+void Socket::SendDataMessage()
+{
+	//Ñòðîêà äëÿ ñîîáùåíèÿ ïîëüçîâàòåëÿ
+	char message[MAXSTRLEN];
+	//Áåç ýòîãî ìåòîäà èç ïîòîêà áóäåò ñ÷èòàí ïîñëåäíèé
+	//ââîä ïîëüçîâàòåëÿ, âûïîëíÿåì ñáðîñ.
+	cin.ignore();
+	cout << "Input message: ";
+	cin.get(message, MAXSTRLEN);
+	SendData(message);
+}
+
+void Socket::SendDataMessageToClient(char* receiveMessage)
+{
+	int price = 0;
+	if (receiveMessage[0] == '1') {
+		price = 7800;
+	}
+	else if (receiveMessage[0] == '2') {
+		price = 26000;
+	}
+	else {
+		price = 9000;
+	}
+
+	char str_char[10];
+	int rm = int(receiveMessage[2]);
+	int sum = rm * price;
+	cout << "rm = " << rm << endl;
+	cout << "price = " << price << endl;
+	cout << "sum = " << sum << endl;
+
+
+	SendData(str_char);
+}
+
+void ServerSocket::StartHosting(int port)
+{
+	Bind(port);
+	Listen();
+}
+
+void ServerSocket::Listen()
+{
+	cout << "Waiting for client..." << endl;
+
+	//Ïðè îøèáêå àêòèâàöèè ñîêåòà â ðåæèìå ïðîñëóøèâàíèÿ
+	//âûâîäèì îøèáêó
+	if (listen(_socket, 1) == SOCKET_ERROR)
+	{
+		cout << "Listening error\n";
+		system("pause");
+		WSACleanup();
+		exit(15);
+	}
+	/*
+	Ìåòîä ÿâëÿåòñÿ áëîêèðóþùèì. Îæèäàåì ïîäêëþ÷åíèå êëèåíòà.
+	Êàê òîëüêî êëèåíò ïîäêëþ÷èëñÿ, ôóíêöèÿ accept âîçâðàùàåò
+	íîâûé ñîêåò, ÷åðåç êîòîðûé ïðîèñõîäèò îáìåí äàííûìè.
+	*/
+	acceptSocket = accept(_socket, NULL, NULL);
+	while (acceptSocket == SOCKET_ERROR)
+	{
+		acceptSocket = accept(_socket, NULL, NULL);
+	}
+	_socket = acceptSocket;
+}
+
+void ServerSocket::Bind(int port)
+{
+	//Óêàçûâàåì ñåìåéñòâî àäðåñîâ IPv4
+	addr.sin_family = AF_INET;
+	/*Ïðåîáðàçóåì àäðåñ "0.0.0.0"â ïðàâèëüíóþ
+	ñòðóêòóðó õðàíåíèÿ àäðåñîâ, ðåçóëüòàò ñîõðàíÿåì â ïîëå sin_addr */
+	inet_pton(AF_INET, "0.0.0.0", &addr.sin_addr);
+	//Óêàçûâàåì ïîðò. 
+	//Ôóíêèöÿ htons âûïîëíÿåò ïðåîáðàçîâàíèå ÷èñëà â
+	//ñåòåâîé ïîðÿäîê áàéò
+	addr.sin_port = htons(port);
+
+	cout << "Binding to port:  " << port << endl;
+	//Ïðè íåóäà÷íîì áèíäèíãå ê ïîðòó, âûâîäèì ñîîáùåíèå ïðî îøèáêó
+	if (bind(_socket, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR)
+	{
+		cout << "Failed to bind to port\n";
+		system("pause");
+		WSACleanup();
+		exit(14);
+	}
+}
+
+void ClientSocket::ConnectToServer(const char* ipAddress, int port)
+{
+	addr.sin_family = AF_INET;
+	inet_pton(AF_INET, ipAddress, &addr.sin_addr);
+	addr.sin_port = htons(port);
+	//ïðè íåóäà÷íîì ïîäêëþ÷åíèè ê ñåðâåðó âûâîäèì ñîîáùåíèå ïðî îøèáêó
+	if (connect(_socket, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR)
+	{
+		cout << "Failed to connect to server\n";
+		system("pause");
+		WSACleanup();
+		exit(13);
+	}
+}
